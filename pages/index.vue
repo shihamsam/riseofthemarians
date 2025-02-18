01bbd795e9ac8d3ddeaef23aad6f7106f758ebc4
Shiham Samsudeen <shihams247@gmail.com>
2025-02-18 18:16:47 +0530

    Opacity adjusted

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@ definePageMeta({
     class="min-h-screen bg-gray-900 flex flex-col items-center justify-center relative px-4"
   >
     <div
-      class="absolute top-0 left-0 w-full h-full bg-cover bg-center opacity-70"
+      class="absolute top-0 left-0 w-full h-full bg-cover bg-center opacity-100"
       style="
         background-image: url('/underconstruction/riseofthemarians-backdrop.jpg');
       "
