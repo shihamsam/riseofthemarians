ed9d8fe0e05b74a7a8d409c14e8a6bdff56a91d3
Shiham Samsudeen <shihams247@gmail.com>
2025-04-26 23:24:22 +0530

    Navbar Auto Closing on Mobile Fixed

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { NuxtLink } from "#components";
+
+// Reactive state for mobile menu visibility
+const isMenuOpen = ref(false);
+
+// Function to toggle the menu
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+
+// Function to close the menu
+const closeMenu = () => {
+  isMenuOpen.value = false;
+};
 </script>
 
 <template>
@@ -29,7 +43,7 @@ import { NuxtLink } from "#components";
           DONATE
         </NuxtLink>
         <button
-          data-collapse-toggle="navbar-sticky"
+          @click="toggleMenu"
           type="button"
           class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-sticky"
@@ -54,7 +68,8 @@ import { NuxtLink } from "#components";
         </button>
       </div>
       <div
-        class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+        :class="{ hidden: !isMenuOpen, block: isMenuOpen }"
+        class="items-center justify-between w-full md:flex md:w-auto md:order-1"
         id="navbar-sticky"
       >
         <ul
@@ -63,8 +78,9 @@ import { NuxtLink } from "#components";
           <li>
             <NuxtLink
               to="/"
+              @click="closeMenu"
               class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              active-class="bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+              active-class="text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
               aria-current="page"
               >Home</NuxtLink
             >
@@ -72,24 +88,27 @@ import { NuxtLink } from "#components";
           <li>
             <NuxtLink
               to="/project"
+              @click="closeMenu"
               class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              active-class="bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+              active-class="text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
               >Our Projects</NuxtLink
             >
           </li>
           <li>
             <NuxtLink
               to="/work"
+              @click="closeMenu"
               class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              active-class="bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+              active-class="text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
               >Gallery</NuxtLink
             >
           </li>
           <li>
             <NuxtLink
               to="/committee"
+              @click="closeMenu"
               class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              active-class="bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+              active-class="text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
               >Committee</NuxtLink
             >
           </li>
